53b4da7e5fd1e876ec592ba0b08d3c233cdcfa37
kwhit2 <kfw2@outlook.com>
2021-07-13 15:22:19 +0000

    Adding Task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                if (alphabet != 'e' && alphabet != 'q')
+                {
+                Console.Write(alphabet);
+                }
+            }
+        }
+    }
+}
