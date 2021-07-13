ced76d7fb50d7d39d65a04dbf4375c809a826c98
kwhit2 <kfw2@outlook.com>
2021-07-13 15:48:11 +0000

    Adding Task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    // Returns True if c is lowercase, Returns False otherwise
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            return (true);
+        }
+        else
+        {
+            return (false);
+        }
+    }
+}
