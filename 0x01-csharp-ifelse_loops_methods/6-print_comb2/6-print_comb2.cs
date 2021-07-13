56d098f87a76182aa803ecbb2456b84af4b4ffb3
kwhit2 <kfw2@outlook.com>
2021-07-13 18:27:59 +0000

    Adding Task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d1;
+            int d2;
+
+            for (d1 = 0; d1 <= 9; d1++)
+            {
+                for (d2 = d1 + 1; d2 <= 9; d2++)
+                {
+                    Console.Write(d1.ToString() + d2.ToString());
+                    if (d1 == 8 && d2 == 9)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
