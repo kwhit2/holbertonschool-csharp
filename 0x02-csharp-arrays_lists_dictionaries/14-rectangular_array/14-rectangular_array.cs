1f79fcccd9c0aa7703574b8b15239460c609246f
kwhit2 <kfw2@outlook.com>
2021-07-14 19:06:07 +0000

    Adding Task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] twoD = new int [5,5] {{0,0,0,0,0}, {0,0,0,0,0}, {0,0,1,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+            int rowLength = twoD.GetLength(0);
+            int colLength = twoD.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    if (j == colLength - 1)
+                    {
+                        Console.Write(string.Format("{0}", twoD[i, j]));
+                    }
+                    else
+                    {
+                        Console.Write(string.Format("{0} ", twoD[i, j]));
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
