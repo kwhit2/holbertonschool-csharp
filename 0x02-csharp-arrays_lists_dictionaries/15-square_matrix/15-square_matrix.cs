bcd8e445e897a070d8298a6d7317f01406eb6da7
kwhit2 <kfw2@outlook.com>
2021-07-14 19:22:16 +0000

    Adding Task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int [,] twoD = myMatrix;
+        int rowLength = twoD.GetLength(0);
+        int colLength = twoD.GetLength(1);
+        for (int i = 0; i < rowLength; i++)
+        {
+            for (int j = 0; j < colLength; j++)
+            {
+                twoD[i, j] *= twoD[i, j];
+            }
+        }
+        return (twoD);
+    }
+}
