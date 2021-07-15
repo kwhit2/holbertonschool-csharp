00ff3d188453cc6a53ddec374308d592643c6c9e
kwhit2 <kfw2@outlook.com>
2021-07-15 17:18:23 +0000

    Adding Task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+        Console.WriteLine("Top item: {0}", aStack.Peek());
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+        if (aStack.Contains(search))
+        {
+            int searchIndex = aStack.ToArray().ToList().IndexOf(search);  // how to get index of element at a linked list
+            while (aStack.Count > searchIndex - 1)  // - 1 because an extra one was being printed
+            {
+                aStack.Pop();
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
