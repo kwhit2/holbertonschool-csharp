a83cba378fa09669d390f67f2a5e72587d959128
kwhit2 <kfw2@outlook.com>
2021-07-15 18:13:09 +0000

    Adding Task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> intList = new LinkedList<int>();
+        for (int i = size - 1; i >= 0; i--)
+        {
+            intList.AddFirst(i);
+        }
+        foreach (int value in intList)
+        {
+            Console.WriteLine(value.ToString());
+        }
+        return intList;
+    }
+}
