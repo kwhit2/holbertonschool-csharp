3d4fc4cb34d6d655b4a074ce66cc23308aca9eb1
kwhit2 <kfw2@outlook.com>
2021-07-15 16:51:46 +0000

    Adding Task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> combined = new List<int>();
+        foreach (int i in list1)
+        {
+            if (list2.Contains(i))
+            {
+                continue;
+            }
+            else
+            {
+                combined.Add(i);
+            }
+        }
+        foreach (int k in list2)
+        {
+            if (list1.Contains(k))
+            {
+                continue;
+            }
+            else
+            {
+                combined.Add(k);
+            }
+        }
+        combined.Sort();
+        return (combined);
+    }
+}
