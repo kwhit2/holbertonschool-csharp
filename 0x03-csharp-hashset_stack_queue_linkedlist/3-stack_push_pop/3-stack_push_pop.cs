481415313620e366fd60a2740c8a9d204b499ed8
kwhit2 <kfw2@outlook.com>
2021-07-15 17:27:28 +0000

    Hopeful Fix

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,12 +7,14 @@ class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: " + aStack.Count);
-        Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        if (aStack.Count != 0)
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
         if (aStack.Count == 0)
         {
             Console.WriteLine("Stack is empty");
-            Console.WriteLine("Top item: {0}", aStack.Peek());
-            Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
         }
         Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
         if (aStack.Contains(search))
