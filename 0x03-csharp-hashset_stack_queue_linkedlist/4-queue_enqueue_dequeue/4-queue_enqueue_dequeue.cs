034a7bb69531297a1ce814bf0ee71435eb9678ab
kwhit2 <kfw2@outlook.com>
2021-07-15 17:40:15 +0000

    Adding Task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aQueue.Count);
+
+        if (aQueue.Count != 0)
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        if (aQueue.Contains(search))
+        {
+            int searchIndex = aQueue.ToArray().ToList().IndexOf(search);  // how to get index of element at a linked list
+            while (aQueue.Count > searchIndex - 1)  // - 1 because an extra one was being printed
+            {
+                aQueue.Dequeue();
+            }
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
