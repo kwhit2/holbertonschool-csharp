aac101136cbe861feb8889c447a04448f65a2300
kwhit2 <kfw2@outlook.com>
2021-07-15 13:36:48 +0000

    Adding Task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        List<int> uniqIntList = new List<int>();
+        uniqIntList = myList.Distinct().ToList();
+        for (int i = 0; i < uniqIntList.Count; i++)
+        {
+            sum += uniqIntList[i];
+        }
+        return sum;
+    }
+}
