41d0949dc76ac3107609155f0ad43dfe07e635e2
Kyle Whitten <kfw2@outlook.com>
2021-07-27 09:32:40 -0500

    Task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Public class that defines a Zombie</summary>
+    class Zombie
+    {
+        public int health;
+
+        ///<summary>Public constructor that defines a Zombies health</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
