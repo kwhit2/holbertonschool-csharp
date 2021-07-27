a8548817d301e10827feec9e267151db51419f65
Kyle Whitten <kfw2@outlook.com>
2021-07-27 09:57:35 -0500

    Adjusted for private field _health

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -5,18 +5,18 @@ namespace Enemies
     ///<summary>Public class that defines a Zombie</summary>
     class Zombie
     {
-        private int health;
+        private int _health;
 
         ///<summary>Public constructor that defines a Zombies health</summary>
         public Zombie()
         {
-            health = 0;
+            _health = 0;
         }
 
         ///<summary>Public constructor that throws ArgumentException if health if less than 0</summary>
         public Zombie(int value)
         {
-            health = value;
+            _health = value;
             if (value < 0)
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");
@@ -25,7 +25,7 @@ namespace Enemies
         ///<summary>Public method that returns the value of health of the Zombie object</summary>
         public int GetHealth()
         {
-            return health;
+            return _health;
         }
     }
 }
