43ba00dae6bf15ac65ea934b2b12047575db5008
Kyle Whitten <kfw2@outlook.com>
2021-07-27 09:43:13 -0500

    Task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Public class that defines a Zombie</summary>
+    class Zombie
+    {
+        public int health;
+
+        ///<summary>Public constructor that defines a Zombies health</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>Public constructor that throws ArgumentException if health if less than 0</summary>
+        public Zombie(int value)
+        {
+            health = value;
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
