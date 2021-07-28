88df063db34bf5243a7a93318936810a1f2bf066
Kyle Whitten <kfw2@outlook.com>
2021-07-28 11:19:42 -0500

    Bones

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Matrix Class</summary>
+    public class Matrix
+    {
+        ///<summary>divides all elements of a matrix</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            
+        }
+    }
+}
