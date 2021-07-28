f37e4a281920e3226acc5c2006eec0d38c10968d
Kyle Whitten <kfw2@outlook.com>
2021-07-28 15:07:14 -0500

    Bones

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    ///<summary>Tests class</summary>
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Str Class</summary>
+    public class Str
+    {
+        ///<summary>CamelCase method: determines how many words are in a string.</summary>
+        public static int CamelCase(string s)
+        {
+
+        }
+    }
+}
