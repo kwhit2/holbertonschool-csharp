660c68ec5afd921606fc01188ede3b62ca7e7b45
Kyle Whitten <kfw2@outlook.com>
2021-07-28 11:40:35 -0500

    It passed!

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -2,12 +2,28 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    [TestFixture]
+    ///<summary>Tests class</summary>
     public class Tests
     {
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            int[,] myMatrix = new int[,]
+            {
+                {2, 4, 6},
+                {8, 10, 12}
+            };
+
+            int[,] output = Matrix.Divide(myMatrix, 2);
+            
+            int[,] resultMatrix = new int[,]
+            {
+                {1, 2, 3},
+                {4, 5, 6}
+            };
+
+            Assert.AreEqual(resultMatrix, output);
         }
     }
 }
--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -8,7 +8,29 @@ namespace MyMath
         ///<summary>divides all elements of a matrix</summary>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            
+            int [,] newMatrix = matrix;
+            int rowLength = newMatrix.GetLength(0);
+            int colLength = newMatrix.GetLength(1);
+            if (matrix == null)
+            {
+                return null;
+            }
+            try
+            {
+                for (int i = 0; i < rowLength; i++)
+                {
+                    for (int j = 0; j < colLength; j++)
+                    {
+                        newMatrix[i, j] /= num;
+                    }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return (newMatrix);
         }
     }
 }
