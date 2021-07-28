fab8c9fcc9c88fb8546eca8190afdcf1a1b1f859
Kyle Whitten <kfw2@outlook.com>
2021-07-28 14:58:45 -0500

    Bones

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Str Class</summary>
+    public class Str
+    {
+        ///<summary>UniqueChar method: returns the index of the first non-repeating character of a string.</summary>
+        public static int UniqueChar(string s)
+        {
+
+        }
+    }
+}
