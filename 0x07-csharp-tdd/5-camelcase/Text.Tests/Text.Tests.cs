fcfa680e32c9dda60b213d7d6999b5cbe82d493d
Kyle Whitten <kfw2@outlook.com>
2021-07-29 11:16:36 -0500

    Added Tests

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -32,5 +32,32 @@ namespace Text.Tests
 
             Assert.AreEqual(4, output);
         }
+
+        [Test]
+        public void TestOneWord()
+        {
+            string myString = "dog";
+            int output = Str.CamelCase(myString);
+
+            Assert.AreEqual(1, output);
+        }
+
+        [Test]
+        public void TestTwoWords()
+        {
+            string myString = "dog Bad";
+            int output = Str.CamelCase(myString);
+
+            Assert.AreEqual(2, output);
+        }
+
+        [Test]
+        public void TestMultWords()
+        {
+            string myString = "dog Bad Baby good";
+            int output = Str.CamelCase(myString);
+
+            Assert.AreEqual(3, output);
+        }
     }
 }
