0b77b60a248ac235198f8d1ac2de9dadba3a5c5f
Kyle Whitten <kfw2@outlook.com>
2021-07-29 09:59:40 -0500

    Updated Test

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -53,13 +53,13 @@ namespace MyMath.Tests
         {
             List<int> myList = new List<int>();
 
-            myList.Add(1);
+            myList.Add(-1);
             myList.Add(-6);
-            myList.Add(9);
+            myList.Add(-9);
 
             int output = Operations.Max(myList);
 
-            Assert.AreEqual(9, output);
+            Assert.AreEqual(-1, output);
         }
 
         [Test]
