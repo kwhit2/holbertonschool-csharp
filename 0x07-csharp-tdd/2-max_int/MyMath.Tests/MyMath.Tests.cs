cdf75b18f36e4f2b43cdbbe98cc293fd93c36926
Kyle Whitten <kfw2@outlook.com>
2021-07-29 09:56:19 -0500

    Adding Tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -20,5 +20,56 @@ namespace MyMath.Tests
 
             Assert.AreEqual(9, output);
         }
+        [Test]
+        public void Test2()
+        {
+            List<int> myList = new List<int>();
+
+            myList.Add(9);
+            myList.Add(6);
+            myList.Add(1);
+
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(9, output);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            List<int> myList = new List<int>();
+
+            myList.Add(1);
+            myList.Add(9);
+            myList.Add(6);
+
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(9, output);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            List<int> myList = new List<int>();
+
+            myList.Add(1);
+            myList.Add(-6);
+            myList.Add(9);
+
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(9, output);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            List<int> myList = new List<int>();
+
+            int output = Operations.Max(myList);
+
+            Assert.AreEqual(0, output);
+        }
     }
 }
