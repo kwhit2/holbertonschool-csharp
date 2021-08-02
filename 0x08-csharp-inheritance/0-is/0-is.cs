3bf2a6c8ad507d02c28f992c2ffb63dc2650c1bd
Kyle Whitten <kfw2@outlook.com>
2021-08-02 08:04:40 -0500

    Task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Class Obj</summary>
+class Obj
+{
+    ///<summary>Check if Type Int</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+            return true;
+        else
+            return false;
+    }
+}
