bace670bf1cbe922c4d7c22d4d73ff931dc9d84d
Kyle Whitten <kfw2@outlook.com>
2021-08-02 11:12:34 -0500

    Task 4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Class Animal</summary>
+class Animal
+{
+    // Empty class
+}
+
+///<summary>Class Dog: Inherits from empty class Animal</summary>
+class Dog : Animal
+{
+    // Empty class
+}
