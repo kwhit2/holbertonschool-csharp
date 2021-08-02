1c8794154db8de32c62a255f6331b1295b72b318
Kyle Whitten <kfw2@outlook.com>
2021-08-02 10:15:10 -0500

    Task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+///<summary>Class Obj</summary>
+class Obj
+{
+    ///<summary>Print method: prints the names of the available properties and methods of an object</summary>
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] kPropInfo;
+        MethodInfo[] kMethInfo;
+
+        kPropInfo = myObj.GetType().GetProperties();
+        kMethInfo = myObj.GetType().GetMethods();
+
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+
+        for (int i = 0; i < kPropInfo.Length; i++)
+        {
+            Console.WriteLine(kPropInfo[i].Name);
+        }
+
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+
+        for (int i = 0; i < kMethInfo.Length; i++)
+        {
+            Console.WriteLine(kMethInfo[i].Name);
+        }
+    }
+}
