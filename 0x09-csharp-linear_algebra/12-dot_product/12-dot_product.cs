d66918edd6c4f7ba5e586a6fb5ce5501bc1da260
Kyle Whitten <kfw2@outlook.com>
2021-08-16 18:48:14 -0500

    Task bones

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Vector Math class</summary>
+class VectorMath
+{
+    ///<summary>method that calculates dot product of either two 2D or two 3D vectors.</summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
