71a15f16f04347c828e8148a247b4d380a3cdc9e
Kyle Whitten <kfw2@outlook.com>
2021-08-17 15:19:36 -0500

    Bones

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>MatrixMath class</summary>
+class MatrixMath
+{
+    ///<summary>method that multiplies a matrix and a scalar and returns the resulting matrix.</summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
