a7f815aa2d530ff062887a9d0377d403777ac6f9
Kyle Whitten <kfw2@outlook.com>
2021-08-17 10:08:01 -0500

    Task 12

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -6,6 +6,19 @@ class VectorMath
     ///<summary>method that calculates dot product of either two 2D or two 3D vectors.</summary>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
-        Console.WriteLine("Hello World!");
+        if (vector1.Length == 2 && vector2.Length ==2)
+        {
+            double dotProd = ((vector1[0] * vector2[0]) + (vector1[1] * vector2[1]));
+            return Math.Round(dotProd, 2);
+        }
+        if (vector1.Length == 3 && vector2.Length ==3)
+        {
+            double dotProd = ((vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + vector1[2] * vector2[2]);
+            return Math.Round(dotProd, 2);
+        }
+        else
+        {
+            return -1;
+        }
     }
 }
