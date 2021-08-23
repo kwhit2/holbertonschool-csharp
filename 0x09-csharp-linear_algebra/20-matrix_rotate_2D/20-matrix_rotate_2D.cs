bdeab1344df23f03e5123012484ef4295cd207d4
Kyle Whitten <kfw2@outlook.com>
2021-08-23 10:33:39 -0500

    Task 20

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+///<summary>MatrixMath class</summary>
+class MatrixMath
+{
+    ///<summary>method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
+    // resource : https://math.stackexchange.com/questions/1404484/how-is-that-a-rotation-by-an-angle-%CE%B8-about-the-origin-can-be-represented-by-this
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] defaultMatrix = new double[,] {{-1}};
+        double[,] twoDmatrix = new double [2,2];
+        double[,] rotate = new double[2, 2] {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}}; 
+        int r = matrix.GetLength(0);
+        int c = matrix.GetLength(1);
+
+        if (r != 2 || c != 2)
+        {
+           return defaultMatrix;
+        }
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    twoDmatrix[i, j] = Math.Round(twoDmatrix[i, j] + (matrix[i, k] * rotate[k, j]), 2);
+                }
+            }
+        }
+        return twoDmatrix;
+    }
+}
+
