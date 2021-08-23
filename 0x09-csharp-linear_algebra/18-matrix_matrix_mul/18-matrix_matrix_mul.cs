27a1f84e2f8fd145f818686d70f2a2f2d7d4cb22
Kyle Whitten <kfw2@outlook.com>
2021-08-23 09:56:28 -0500

    Task 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+///<summary>MatrixMath class</summary>
+class MatrixMath
+{
+    ///<summary>method that multiplies two matrices and returns the resulting matrix</summary>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] defaultMatrix = new double[,] {{-1}};
+        int rA = matrix1.GetLength(0);
+        int cA = matrix1.GetLength(1);
+        int rB = matrix2.GetLength(0);
+        int cB = matrix2.GetLength(1);
+        double temp = 0;
+        double[,] kHasil = new double[rA, cB];
+
+        if (cA != rB)
+        {
+            return defaultMatrix;
+        }
+        else
+        {
+            for (int i = 0; i < rA; i++)
+            {
+                for (int j = 0; j < cB; j++)
+                {
+                    temp = 0;
+                    for (int k = 0; k < cA; k++)
+                    {
+                        temp += matrix1[i, k] * matrix2[k, j];
+                    }
+                    kHasil[i, j] = temp;
+                }
+            }
+        return kHasil;
+        }
+    }
+}
