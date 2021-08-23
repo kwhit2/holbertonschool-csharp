17738b3fbf57bce55ce82d31e347161ad7fdd71e
Kyle Whitten <kfw2@outlook.com>
2021-08-23 13:17:02 -0500

    Fixed for loops

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -8,9 +8,9 @@ class MatrixMath
     {
         double[,] returnMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int j = 0; j < 2; j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 returnMatrix[j, i] = matrix[i, j];
             }
