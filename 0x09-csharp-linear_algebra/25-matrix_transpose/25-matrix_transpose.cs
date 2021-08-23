f954021e528bcc892a7c0c7cc11be359eefd154a
Kyle Whitten <kfw2@outlook.com>
2021-08-23 13:13:00 -0500

    Task 23

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+///<summary>MatrixMath class</summary>
+class MatrixMath
+{
+    ///<summary>method to transpose a matrix and return the resulting matrix.</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] returnMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                returnMatrix[j, i] = matrix[i, j];
+            }
+        }
+        return returnMatrix;
+    }
+}
