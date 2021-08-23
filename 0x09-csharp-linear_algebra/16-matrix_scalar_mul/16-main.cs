cab4923977ac93ce8778e99a363aaf9bf165e84e
Kyle Whitten <kfw2@outlook.com>
2021-08-23 09:43:18 -0500

    Task 16

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-main.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-main.cs
@@ -8,11 +8,12 @@ class Program
         double[,] matrix2d2 = { { 1, 7 }, { -8, -2 } };
         double[,] matrix3d1 = { { 14, -3, 0 }, { -11, -5, 3 }, { 2, -9, 13 } };
         double[,] matrix3d2 = { { 6, 16, 21 }, { 5, 2, 0 }, { 1, 3, 7 } };
+        double scalar = 4;
         double[,] result2d;
         double[,] result3d;
 
-        result2d = MatrixMath.Add(matrix2d1, matrix2d2);
-        result3d = MatrixMath.Add(matrix3d1, matrix3d2);
+        result2d = MatrixMath.MultiplyScalar(matrix2d1, scalar);
+        result3d = MatrixMath.MultiplyScalar(matrix3d1, scalar);
 
         for (int i = 0; i < result2d.GetLength(0); i++)
         {
--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -6,6 +6,28 @@ class MatrixMath
     ///<summary>method that multiplies a matrix and a scalar and returns the resulting matrix.</summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        Console.WriteLine("Hello World!");
+        double[,] defaultMatrix = new double[,] {{-1}};
+        double[,] returnMatrix;
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            returnMatrix = new double[2,2];
+        }
+        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            returnMatrix = new double[3,3];
+        }
+        else
+        {
+            return defaultMatrix;
+        }
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                returnMatrix[i, j] = matrix[i, j] * scalar; 
+            }
+        }
+        return returnMatrix;
     }
 }
