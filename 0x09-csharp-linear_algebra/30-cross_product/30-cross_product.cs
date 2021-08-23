42bb9589dd7ec5fa952ae230aee0dba3d20b2b6e
Kyle Whitten <kfw2@outlook.com>
2021-08-23 13:47:44 -0500

    Task 28

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+///<summary>MatrixMath class</summary>
+class VectorMath
+{
+    ///<summary>method that calculates the cross product of two 3D vectors and returns the resulting vector.</summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] crossProduct = {0, 0, 0};
+        double[] not3d = new double[1] {-1};
+
+        if (!matrix.GetLength(0) == 3 || !matrix.GetLength(1) == 3)
+        {
+            return not3d;
+        }
+        crossProduct = CrossProduct(vector1, vector2);
+        return crossProduct;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-main.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {       
+
+    }
+}
+       
