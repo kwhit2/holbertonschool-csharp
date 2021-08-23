477a705d2eb18fdbeb0b12360f635501c35c118d
Kyle Whitten <kfw2@outlook.com>
2021-08-23 13:26:41 -0500

    Task 26

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {           
+        double[,] matrix = new double[,] {{-1}};
+
+        Console.WriteLine(MatrixMath.Determinant(matrix));
+    }
+}
+       
