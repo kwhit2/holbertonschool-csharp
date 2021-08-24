7a52b90a1f87b51eb81605656dd6a63d3c858853
Kyle Whitten <kfw2@outlook.com>
2021-08-24 09:19:12 -0500

    Task 31

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -6,13 +6,21 @@ class MatrixMath
     ///<summary>method that calculates the inverse of a 2D matrix and returns the resulting matrix.</summary>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double[,] not2d = new double[,] {{-1}};
-        double[,] nonInvertible = new double[,] {{-1}};
-
-        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-        {
-            return not2d;
-        }
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
         
+        if (row != 2 && column != 2)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double det = (matrix[0,0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        if (det == 0)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double[,] inverse = new double[,] {{Math.Round((1 / det) * matrix[1, 1], 2), Math.Round((1 / det) * -matrix[0, 1], 2)}, {Math.Round((1 / det) * -matrix[1, 0], 2), Math.Round((1 / det) * matrix[0, 0],2)}};
+        return inverse;
     }
 }
