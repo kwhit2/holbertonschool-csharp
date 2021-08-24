e429b751aa408b4778446d1dcc058b23e7704f75
Kyle Whitten <kfw2@outlook.com>
2021-08-24 07:37:45 -0500

    Bones. Testing Git

---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+///<summary>MatrixMAth class</summary>
+class MatrixMath
+{
+    ///<summary>method that calculates the inverse of a 2D matrix and returns the resulting matrix.</summary>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        double[,] not2d = new double[,] {{-1}};
+        double[,] nonInvertible = new double[,] {{-1}};
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            return not2d;
+        }
+        
+    }
+}
