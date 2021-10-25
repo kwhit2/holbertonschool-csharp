c63e6ea53ce4f5a45dfd4e136c09a24e5fb9e5b1
Kyle Whitten <kfw2@outlook.com>
2021-10-25 08:53:25 -0500

    Task 2

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+///<summary> class Queue </summary>
+class Queue<T>
+{
+    public Node head = null;
+    public Node tail = null;
+    public int count;
+
+    ///<summary> CheckType method </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    ///<summary> Class Node Method </summary>
+    public class Node
+    {
+        public T data;
+        public Node next;
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+
+    ///<summary> Enqueue() Method </summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else if (tail == null)
+        {
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    ///<summary> Count() Method </summary>
+    public int Count()
+    {
+        return count;
+    }
+
+    ///<summary> Dequeue() Method </summary>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            return default(T);
+        }
+        else
+        {
+            T value = head.data;
+            head = head.next;
+            count--;
+            return value;
+        }
+    }
+}
