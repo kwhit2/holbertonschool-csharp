c89135441421efbe72d59bef9d017d45bad6275b
Kyle Whitten <kfw2@outlook.com>
2021-10-25 08:44:26 -0500

    README, gitignore, task 0 & 1

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary> class Queue </summary>
+class Queue<T>
+{
+    ///<summary> CheckType method </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+///<summary> class Queue </summary>
+class Queue<T>
+{
+    public Node head = null;
+    public Node tail = null;
+    public int count;
+
+    ///<summary> CheckType method </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    ///<summary> Class Node Method </summary>
+    public class Node
+    {
+        public T data;
+        public Node next;
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+
+    ///<summary> Enqueue() Method </summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else if (tail == null)
+        {
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    ///<summary> Count() Method </summary>
+    public int Count()
+    {
+        return count;
+    }
+}
