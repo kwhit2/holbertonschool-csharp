b5615209990b9abacd4700cb040e69873d8ed339
Kyle Whitten <kfw2@outlook.com>
2021-10-26 08:42:03 -0500

    Task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+///<summary> abstract class Base</summary>
+public abstract class Base
+{
+    ///<summary> public property name</summary>
+    public string name { get; set; }
+
+    ///<summary> ToString() override method</summary>
+    public override string ToString()
+    {
+        return (this.name) + " is a " + (this.GetType());
+    }
+}
